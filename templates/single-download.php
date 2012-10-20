05a4bea288bf08c9638bc6a85f4dd21203b74a36
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 17:56:24 +0300

    removing addthis from single downloads

--- templates/single-download.php
+++ templates/single-download.php
@@ -11,14 +11,6 @@
           <?php } ?>
           <h1 itemprop="name" class="mp_product_name" class="entry-title"><?php the_title(); ?></h1>
           
-          <div class="addthis_toolbox addthis_default_style ">
-            <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-            <a class="addthis_button_tweet"></a>
-            <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
-            <a class="addthis_counter addthis_pill_style"></a>
-          </div>
-          <script type="text/javascript">var addthis_config = { "data_track_addressbar":true };</script>
-          <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4e8994de6a13ee6a"></script>
         </header>
 
         <div class="product-meta content clearfix">
