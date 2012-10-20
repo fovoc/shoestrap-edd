2a959209ebb2b39e1c6738c21673f508a4f2fd28
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 18:51:26 +0300

    including less php compiler

--- edd.php
+++ edd.php
@@ -8,10 +8,15 @@ Author: Aristeides Stathopoulos
 Author URI: http://aristeides.com
 */
 
+
 require_once dirname( __FILE__ ) . '/template-functions.php';           // Include template files for the appropriate post types etc.
 require_once dirname( __FILE__ ) . '/includes/customizer-output.php';   // Some Extra customizer functions
 require_once dirname( __FILE__ ) . '/templates/cart-button.php';        // The Cart button template (for the navbar)
 
+// Include the less compiler if not present
+if (!class_exists(lessc)){
+  require_once dirname( __FILE__) . '/includes/lessphp/lessc.inc.php';
+}
 
 function bc_edd_enqueue_resources() {
   wp_enqueue_style('bc_edd_styles', plugins_url('assets/css/styles.css', __FILE__), false, null);
