21c5c5d2026fe9a2ffad0bd0c1f357521ab3a174
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 17:57:28 +0300

    adding template functions & correctly enqueuing scripts & styles

--- edd.php
+++ edd.php
@@ -8,8 +8,12 @@ Author: Aristeides Stathopoulos
 Author URI: http://aristeides.com
 */
 
+require_once dirname( __FILE__ ) . '/template-functions.php';           // Include template files for the appropriate post types etc.
 require_once dirname( __FILE__ ) . '/includes/customizer-output.php';   // Some Extra customizer functions
 require_once dirname( __FILE__ ) . '/templates/cart-button.php';        // The Cart button template (for the navbar)
 
 
-wp_enqueue_style('bc_edd_styles', plugins_url('assets/css/styles.css', __FILE__), false, null);
+function bc_edd_enqueu_resources() {
+  wp_enqueue_style('bc_edd_styles', plugins_url('assets/css/styles.css', __FILE__), false, null);
+}
+add_action('wp_enqueue_scripts', 'bc_edd_enqueu_resources', 102);
