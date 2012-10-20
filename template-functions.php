d7ede9b745707d54a88a75653989e796364bb25b
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 18:21:47 +0300

    small bugfix

--- template-functions.php
+++ template-functions.php
@@ -4,8 +4,8 @@ function bc_edd_load_single_template($template) {
   global $wp_query, $post;
  
   if ($wp_query->is_single && $wp_query->query_vars['post_type'] == 'download') { 
-    return dirname(__FILE__) . '/templates/single-download.php'; 
-  return $template; 
+    return dirname(__FILE__) . '/templates/single-download.php';
   }
+  return $template;
 }
 add_filter('template_include', 'bc_edd_load_single_template', 1, 1); 
