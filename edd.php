d8eb6bfdf659be5e252ae3f2f829eba7de3c11d7
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 14:32:49 +0300

    addinf basic plugin info

---
+++ edd.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Bootstrap Commerce Easy Digital Downloads Plugin
+Plugin URI: http://bootstrap-commerce.com
+Description: To be used with the Bootstrap Customizer Core theme
+Version: 1.0
+Author: Aristeides Stathopoulos
+Author URI: http://aristeides.com
+*/
