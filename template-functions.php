917d876b989e16ce7062323aed7b003a7d43daeb
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 18:17:38 +0300

    correctly referencing the template file for single downloads

--- template-functions.php
+++ template-functions.php
@@ -1,19 +1,11 @@
 <?php
   
-function bc_edd_load_single_template() {
+function bc_edd_load_single_template($template) {
   global $wp_query, $post;
-
-  // Only run on single pages of post type = download
-  if ($wp_query->is_single && $wp_query->query_vars['post_type'] == 'download') {
-
-    //only filter public side
-    if (is_admin()){
-      return;
-    } else {
-      
-      // Include the actual template file
-      include_once dirname( __FILE__ ) . '/templates/single-download.php';
-    }
+ 
+  if ($wp_query->is_single && $wp_query->query_vars['post_type'] == 'download') { 
+    return dirname(__FILE__) . '/templates/single-download.php'; 
+  return $template; 
   }
 }
-add_action('template_redirect', 'bc_edd_load_single_template');
+add_filter('template_include', 'bc_edd_load_single_template', 1, 1); 
