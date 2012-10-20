4ec8b524f7472ef1c6375bb09cedca9516dff5dc
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-20 14:49:48 +0300

    fixing the cart-button

--- edd.php
+++ edd.php
@@ -8,5 +8,5 @@ Author: Aristeides Stathopoulos
 Author URI: http://aristeides.com
 */
 
-require_once dirname( __FILE__ ) . '/includes/customizer-output.php';   // Cart button for the navbar
+require_once dirname( __FILE__ ) . '/includes/customizer-output.php';   // Some Extra customizer functions
 require_once dirname( __FILE__ ) . '/templates/cart-button.php';        // The Cart button template (for the navbar)
--- templates/cart-button.php
+++ templates/cart-button.php
@@ -1,1 +1,9 @@
-<a class="btn-cart" href="<?php echo edd_get_checkout_uri(); ?>"><i class="icon-shopping-cart"></i> <?php _e( 'My Cart', 'bootstrap_commerce' ) ?></a>
+<?php
+
+if ( function_exists( 'edd_get_checkout_uri' ) ) {
+  
+  function bc_edd_add_cart_to_navbar() { ?>
+    <a class="btn btn-cart" href="<?php echo edd_get_checkout_uri(); ?>"><i class="icon-shopping-cart"></i> <?php _e( 'My Cart', 'bootstrap_commerce' ) ?></a>
+  <?php }
+  add_action( 'bc_core_nav_top_right', 'bc_edd_add_cart_to_navbar' );
+}
