e6f1ca5f9743fb484ee2b0d062d8ade3d690edf0
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-21 11:28:03 +0300

    adding category and tags template rules

--- template-functions.php
+++ template-functions.php
@@ -2,10 +2,17 @@
   
 function bc_edd_load_single_template($template) {
   global $wp_query, $post;
- 
+  
+  // single download template
   if ($wp_query->is_single && $wp_query->query_vars['post_type'] == 'download') { 
     return dirname(__FILE__) . '/templates/single-download.php';
   }
+  
+  // downloads category and tag template
+  if ( isset( $wp_query->query_vars['taxonomy'] ) && ( $wp_query->query_vars['taxonomy'] == 'download_category' || $wp_query->query_vars['taxonomy'] == 'download_tag' ) ) {
+    return dirname(__FILE__) . '/templates/edd_list.php';
+  }
+
   return $template;
 }
 add_filter('template_include', 'bc_edd_load_single_template', 1, 1); 
