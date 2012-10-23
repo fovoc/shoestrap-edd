1df8f18b6bbb20951f11b55882adce32be1443e9
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-24 02:41:37 +0300

    added quantity in cart via ajax

--- templates/cart-button.php
+++ templates/cart-button.php
@@ -3,7 +3,7 @@
 if ( function_exists( 'edd_get_checkout_uri' ) ) {
   
   function shoestrap_add_cart_to_navbar() { ?>
-    <a class="btn btn-cart" href="<?php echo edd_get_checkout_uri(); ?>"><i class="icon-shopping-cart"></i> <?php _e( 'My Cart', 'shoestrap_edd' ) ?></a>
+    <a class="btn btn-cart" href="<?php echo edd_get_checkout_uri(); ?>"><i class="icon-shopping-cart"></i> <?php _e( 'My Cart', 'shoestrap_edd' ) ?> (<span class="header-cart edd-cart-quantity"><?php echo edd_get_cart_quantity(); ?></span>)</a>
   <?php }
   add_action( 'shoestrap_nav_top_right', 'shoestrap_add_cart_to_navbar' );
 }
