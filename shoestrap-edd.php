dd778897ffea69a59800f7a4d8f4f7215502022d
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-23 16:06:16 +0300

    Adding check to see if the enabled theme is shoestrap (for compativility)

--- shoestrap-edd.php
+++ shoestrap-edd.php
@@ -8,22 +8,23 @@ Author: Aristeides Stathopoulos
 Author URI: http://aristeides.com
 */
 
-
-require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-
-//check if the Bootstrap Commerce Customizer plugin is enabled
-
-require_once dirname( __FILE__ ) . '/template-functions.php';         // Include template files for the appropriate post types etc.
-require_once dirname( __FILE__ ) . '/includes/customizer.php';        // Some Extra customizer functions
-require_once dirname( __FILE__ ) . '/templates/cart-button.php';      // The Cart button template (for the navbar)
-require_once dirname( __FILE__ ) . '/includes/updater/licencing.php'; // Licencing to provide automatic updates
+// Check if the Shoestrap theme is enabled.
+// Only process this plugin if the enabled theme is called shoestrap
+$shoestrap_enabled = wp_get_theme( 'shoestrap' );
+if ( $shoestrap_enabled->exists() ) {
   
-// Include the less compiler if not present
-if (!class_exists('lessc')){
-  require_once dirname( __FILE__) . '/includes/lessphp/lessc.inc.php';
+  require_once dirname( __FILE__ ) . '/template-functions.php';         // Include template files for the appropriate post types etc.
+  require_once dirname( __FILE__ ) . '/includes/customizer.php';        // Some Extra customizer functions
+  require_once dirname( __FILE__ ) . '/templates/cart-button.php';      // The Cart button template (for the navbar)
+  require_once dirname( __FILE__ ) . '/includes/updater/licencing.php'; // Licencing to provide automatic updates
+    
+  // Include the less compiler if not present
+  if (!class_exists('lessc')){
+    require_once dirname( __FILE__) . '/includes/lessphp/lessc.inc.php';
+  }
+  
+  function shoestrap_enqueue_resources() {
+    wp_enqueue_style('shoestrap_styles', plugins_url('assets/css/styles.css', __FILE__), false, null);
+  }
+  add_action('wp_enqueue_scripts', 'shoestrap_enqueue_resources', 102);
 }
-
-function shoestrap_enqueue_resources() {
-  wp_enqueue_style('shoestrap_styles', plugins_url('assets/css/styles.css', __FILE__), false, null);
-}
-add_action('wp_enqueue_scripts', 'shoestrap_enqueue_resources', 102);
